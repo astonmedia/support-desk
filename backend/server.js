49f0b83e2d271ff6c9f064c42456f21031a1b078
Ricky Aston <astonmedia@hotmail.com>
2022-03-12 06:39:19 +1000

    added controller and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("server")
+const express = require("express")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 8000
+const userRoutes = require("./routes/userRoutes")
+
+const app = express()
+
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
