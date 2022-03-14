3326842a1ee48c543d89f3be7e3c5cf93582a96d
Ricky Aston <astonmedia.hotmail.com>
2022-03-14 10:44:46 +1000

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const errorhandler = require("./middleware/errorMiddleware")
 const PORT = process.env.PORT || 8000
 const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use("/api/users", userRoutes)
+
+app.use(errorhandler)
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
