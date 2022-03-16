6a6205449acd129d0bd3c5ea9e4405ab3e6feb08
Ricky Aston <astonmedia@hotmail.com>
2022-03-17 04:47:50 +1000

    finished frontend auth

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -12,8 +12,21 @@ const register = async (UserData) => {
   return response.data
 }
 
+// Login user
+const login = async (UserData) => {
+  const response = await axios.post(API_URL + "/login", UserData)
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data))
+  }
+  return response.data
+}
+
+const logout = () => localStorage.removeItem("user")
+
 const authService = {
   register,
+  logout,
+  login,
 }
 
 export default authService
